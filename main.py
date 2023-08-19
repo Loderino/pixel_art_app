18800e579e49565e6abae8037cf64e38828ac434
Loderino <savostin.c@yandex.ru>
2023-08-19 16:28:03 +0300

    fix: color_buttons size was normalized

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ work_area = Canvas(root, bg='white')
 instruments_panel.pack(side = "top", anchor=NW, fill=X, padx=5, pady=5)
 
 buttons = [
-Button(instruments_panel, height = 10, width = 10, bg="black", command = lambda: change_color("black")),
-Button(instruments_panel, height = 10, width = 10, bg="white", command = lambda: change_color("white")),
-Button(instruments_panel, height = 10, width = 10, bg="purple", command = lambda: change_color("purple")),
-Button(instruments_panel, height = 10, width = 10, bg="green", command = lambda: change_color("green")),
-Button(instruments_panel, height = 10, width = 10, bg="blue", command = lambda: change_color("blue")),
-Button(instruments_panel, height = 10, width = 10, bg="red", command = lambda: change_color("red")),
-Button(instruments_panel, height = 10, width = 10, bg="cyan", command = lambda: change_color("cyan"))]
+Button(instruments_panel, height = 1, width = 1, bg="black", activebackground="black", command = lambda: change_color("black")),
+Button(instruments_panel, height = 1, width = 1, bg="white", activebackground="white", command = lambda: change_color("white")),
+Button(instruments_panel, height = 1, width = 1, bg="purple", activebackground="purple", command = lambda: change_color("purple")),
+Button(instruments_panel, height = 1, width = 1, bg="green", activebackground="green", command = lambda: change_color("green")),
+Button(instruments_panel, height = 1, width = 1, bg="blue", activebackground="blue", command = lambda: change_color("blue")),
+Button(instruments_panel, height = 1, width = 1, bg="red", activebackground="red", command = lambda: change_color("red")),
+Button(instruments_panel, height = 1, width = 1, bg="cyan", activebackground="cyan", command = lambda: change_color("cyan"))]
 for but in buttons:
-    but.pack(side = "left")
+    but.config(highlightbackground="black")
+    but.pack(side = "left", padx = 5)
 work_area.pack(fill="both", side = "top", expand=1)
 
 def set_point(event):
